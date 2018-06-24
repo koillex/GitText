835ae7a4248d99c0f4a92461d9c35ba971efe1ad
我 <604552355@qq.com>
2018-06-24 12:53:42 +0800

    Initial Commit

---
+++ GitText/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitText
+//
+//  Created by koillex on 2018/6/24.
+//  Copyright © 2018年 伍奕帆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
